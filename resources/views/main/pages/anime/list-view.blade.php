92e5c45297e112535da79c49011c33ce5af0b6f6
Mykola Pukhalskyi <pukhalskyi.mykola@gmail.com>
2026-03-21 18:54:24 +0200

    fix: replace removed anime.list-view route with query parameter
    Replace route('anime.list-view') references with
    route('anime.index', ['view' => 'list']) in both grid and
    list view templates. The list-view route was removed when
    view modes were unified into AnimeIndex.

--- resources/views/main/pages/anime/list-view.blade.php
+++ resources/views/main/pages/anime/list-view.blade.php
@@ -150,7 +150,7 @@
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                 </svg>
               </a>
-              <a href="{{ route('anime.list-view') }}" class="p-2 rounded-lg bg-cyan-500/20 text-cyan-400">
+              <a href="{{ route('anime.index', ['view' => 'list']) }}" class="p-2 rounded-lg bg-cyan-500/20 text-cyan-400">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
                 </svg>
